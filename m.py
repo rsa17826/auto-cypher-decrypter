c8a683c4478e337b91d32a108a1d428eddda3e1b
10student <10student@nr.stu>
2025-11-04 13:51:34 -0500

    started

---
+++ m.py
@@ -0,0 +1,4 @@
+import Crypto.Cipher as Cipher
+# https://learning-oreilly-com.eznrcc.vccs.edu/library/view/python-for-security/9781837637553/Text/Chapter_14.xhtml#_idParaDest-330
+# print(type(Cipher.AES))
+# print()
