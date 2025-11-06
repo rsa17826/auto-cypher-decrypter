0acf9583aaf4dbb8c88e2cbe1c41879fa0e7864c
Your Name <you@example.com>
2025-11-06 13:29:39 -0500

    a

--- m.py
+++ m.py
@@ -1,4 +1,27 @@
-import Crypto.Cipher as Cipher
-# https://learning-oreilly-com.eznrcc.vccs.edu/library/view/python-for-security/9781837637553/Text/Chapter_14.xhtml#_idParaDest-330
-# print(type(Cipher.AES))
-# print()
+import Crypto.Cipher.DES as DES
+import Crypto.Cipher.DES3 as DES3
+import Crypto.Cipher.PKCS1_OAEP as PKCS1_OAEP
+import Crypto.Cipher.PKCS1_v1_5 as PKCS1_v1_5
+import Crypto.Cipher.Salsa20 as Salsa20
+import Crypto.Cipher.AES as AES
+import Crypto.Cipher.ARC2 as ARC2
+import Crypto.Cipher.ARC4 as ARC4
+import Crypto.Cipher.Blowfish as Blowfish
+import Crypto.Cipher.CAST as CAST
+import Crypto.Cipher.ChaCha20 as ChaCha20
+import Crypto.Cipher.ChaCha20_Poly1305 as ChaCha20_Poly1305
+
+cypherList = [
+  DES,
+  DES3,
+  PKCS1_OAEP,
+  PKCS1_v1_5,
+  Salsa20,
+  AES,
+  ARC2,
+  ARC4,
+  Blowfish,
+  CAST,
+  ChaCha20,
+  ChaCha20_Poly1305,
+]
